d6a21456c0198c739b562913cd6baf1446e7b710
Amal Raj <amalrajkvdev@gmail.com>
2024-03-03 20:38:57 +0530

    modified:   index.html 	modified:   index.js

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ search.addEventListener('click', () => {
                 image.src = '';
         }
 
-        
+
         temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
         description.innerHTML = `${json.weather[0].description}`;
         humidity.innerHTML = `${json.main.humidity}%`;
